46d457b9ca558833825271033da99d858efb9219
Chirag Kukreja <erkukrejachirag@gmail.com>
2023-04-26 09:16:59 +0530

    Largest Number Among 3 Integers

--- LearningScala/src/LargestNum.scala
+++ LearningScala/src/LargestNum.scala
@@ -9,7 +9,7 @@ object LargestNum extends App {
   //Making sure that a is greater then if will get executed
   if (a > b && a > c) {
     println(a)
-  } //Making sure that b is greater then elseif will get executed
+  } //Making sure that b is greater then else-if will get executed
   else if (b > a && b > c) {
     println(b)
   } //Making sure that c is greater then else part will be executed
