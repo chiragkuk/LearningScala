63e7ebec06c13f41d79ab9727c4391a3737aca87
Chirag Kukreja <erkukrejachirag@gmail.com>
2023-04-30 13:24:42 +0530

    Added Sorting by values

--- LearningSpark/src/WordCount.scala
+++ LearningSpark/src/WordCount.scala
@@ -13,17 +13,22 @@ object WordCount extends App {
   val input = sc.textFile("D:/search_data.txt.txt")
 
   //Performing the transition to split the whole data via space
-  val words = input.flatMap(x => x.split(" "))
+  val words = input.flatMap(_.split(" "))
 
   //Changing the whole data in lower case if there is any upper case
-  val wordsLower = input.flatMap(x => x.toLowerCase())
+  val wordsLower = words.map(_.toLowerCase())
 
   //Now giving the numbers to the each word
-  val wordMap = wordsLower.map(x => (x, 1))
+  val wordMap = wordsLower.map((_, 1))
 
   //Now adding all the occurences of the number
-  val finalCount = wordMap.reduceByKey((a, b) => a + b)
+  val finalCount = wordMap.reduceByKey(_+_)
+  
+  //Sorting the Values
+  
+  val reversedTuple=finalCount.map(x=>(x._2,x._1))
+  val sortedRes=reversedTuple.sortByKey(false).map(x=>(x._2,x._1))
 
   //Collecting the result and printing it
-  finalCount.collect().foreach(println)
+  sortedRes.collect().foreach(println)
 }
