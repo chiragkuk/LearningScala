ba837ce3298c5c79b6d43e817ffff415844ea346
Chirag Kukreja <erkukrejachirag@gmail.com>
2023-04-30 11:46:37 +0530

    Wordcount

---
+++ LearningSpark/src/WordCount.scala
@@ -0,0 +1,27 @@
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
+
+
+object WordCount extends App {
+  //Logger for disabling the errors on the console
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+  // Defining the Spark context rdd to define the usage of cores
+  val sc = new SparkContext("local[*]","wordcount")
+  
+  //Defining the File to be inputted
+  val input = sc.textFile("D:/search_data.txt.txt")
+  
+  //Performing the transition to split the whole data via space
+  val words = input.flatMap(x=>x.split(" "))
+  
+  //Now giving the numbers to the each word
+  val wordMap = words.map(x=>(x,1))
+  
+  //Now adding all the occurences of the number
+  val finalCount = wordMap.reduceByKey((a,b)=>a+b)
+  
+  //Collecting the result and printing it 
+  finalCount.collect().foreach(println)
+}
